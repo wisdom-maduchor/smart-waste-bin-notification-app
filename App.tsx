add932ed8b941515dd1ad58ba8c52ff821e02fc1
wisdom-maduchor <maduchorwisdom@gmail.com>
2026-01-22 23:40:51 +0100

    push update

---
+++ App.tsx
@@ -0,0 +1,16 @@
+// import { View, Text } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>App is running 🚀</Text>
+//     </View>
+//   );
+// }
+
+// // for Expo Router
+// // import { Stack } from 'expo-router';
+
+// // export default function App() {
+// //   return <Stack />;
+// // }
