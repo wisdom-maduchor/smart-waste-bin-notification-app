bea086c4688964edf4a44aa1d86a12cfa959e1f2
wisdom-maduchor <maduchorwisdom@gmail.com>
2026-01-26 20:57:39 +0100

    change again

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import { View, Text } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>App is running 🚀</Text>
-    </View>
-  );
-}
-
-// for Expo Router
-// import { Stack } from 'expo-router';
+// import { View, Text } from 'react-native';
 
 // export default function App() {
-//   return <Stack />;
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>App is running 🚀</Text>
+//     </View>
+//   );
 // }
+
+// for Expo Router
+import { Stack } from 'expo-router';
+
+export default function App() {
+  return <Stack />;
+}
